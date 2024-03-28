c87c586043f1a25c14f2e075bc2570e55443b796
Pushpa Padti <99261071+ppadti@users.noreply.github.com>
2024-03-29 04:16:41 +0530

    Update (#3)
    * upadte
    
    * update

--- Backend/database/connection.go
+++ Backend/database/connection.go
@@ -9,7 +9,7 @@ var DB *sql.DB
 
 func InitDB() {
     var err error
-    DB, err = sql.Open("mysql", "root:Pushpa_123@/todo_list?charset=utf8mb4&parseTime=True&loc=Local")
+    DB, err = sql.Open("mysql", "root:PASSWORD/DB?charset=utf8mb4&parseTime=True&loc=Local")
     if err != nil {
         log.Fatal(err)
     }
